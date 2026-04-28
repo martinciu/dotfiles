b4b74be8157ddb065c3d5b1d0d38c6c1095ea0be
Marcin Ciunelis <marcin.ciunelis@gmail.com>
2026-04-28 13:55:36 +0200

    fix(nvim): use renamed mason-org/mason.nvim repo

--- .config/nvim/lua/plugins/mason-lock.lua
+++ .config/nvim/lua/plugins/mason-lock.lua
@@ -1,7 +1,7 @@
 return {
   {
     "zapling/mason-lock.nvim",
-    dependencies = { "williamboman/mason.nvim" },
+    dependencies = { "mason-org/mason.nvim" },
     init = function()
       require("mason-lock").setup({
         lockfile_path = vim.fn.stdpath("config") .. "/mason-lock.json",
